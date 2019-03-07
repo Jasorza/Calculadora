acb9f787e7d5eb102515feac5e34e7df51fc03b6
Jasorza <41799764+Jasorza@users.noreply.github.com>
2019-03-07 09:52:17 -0500

    3

--- js/funciones.js
+++ js/funciones.js
@@ -1,41 +1,63 @@
 //** Objetos para definir las propiedades de la calculadora */
 var proCal = {
     teclado: document.querySelectorAll('#calculadora ul li'),
-    accion: null
+    accion: null,
+    operaciones: document.querySelector('#calculadora'),
+    digito: 0,
+    cantSigno: 0
 }
 //** Objetos para definir todos los metodos que vamos a definiren la calculadora */
 var metCal = {
     inicio: function () {
-        console.log(proCal.teclado);
+        /*console.log(proCal.teclado);*/
         proCal.teclado.forEach(element => {
             element.addEventListener('click', metCal.oprimirTecla);
         })
     },
     oprimirTecla: function (tecla) {
-        console.log(tecla);
+        /*console.log(tecla);*/
         proCal.accion = tecla.target.getAttribute('class');
-        console.log(proCal.accion);
+        /*console.log(proCal.accion);*/
+        proCal.digito = tecla.target.innerHTML;
+        console.log(proCal.digito);
+        metCal.calculadora(proCal.accion, proCal.digito);
     },
-    calculadora: function (accion) {
+    calculadora: function (accion, digito) {
         switch (accion) {
             case 'numero':
-                console.log(accion);
+                proCal.cantSigno=0;
+                console.log(proCal.cantSigno);
+                if (proCal.operaciones.innerHTML == 0) {
+                    proCal.operaciones.innerHTML = digito;
+                } else {
+                    proCal.operaciones.innerHTML += digito;
+                }
+                proCal.operaciones.innerHTML += digito;
                 break;
             case 'signo':
+                proCal.cantSigno++;
+                console.log(proCal.cantSigno);
+                if (proCal.cantSigno == 1) {
+                    proCal.operaciones.innerHTML += digito;
+                }
+                proCal.operaciones.innerHTML = digito;
                 console.log(accion);
                 break;
             case 'decimal':
                 console.log(accion);
                 break;
             case 'igual':
-            console.log(accion);
+                console.log(accion);
                 break;
             case 'igual':
-            console.log(accion);
+                console.log(accion);
                 break;
             default:
                 break;
         }
+    },
+    borrar: function () {
+        proCal.operaciones.innerHTML = 0;
     }
 }
 metCal.inicio();
